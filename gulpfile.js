63670d35019ee208900e62b5f57f0f6a96962178
mikoloism <mikoloism.github@gmail.com>
2021-11-21 23:00:41 +0330

    [ 🎉 ] version : 2.0.1 [ 🆕 ] dist/ folder added [ 🔨 ] fix gulp-concat with some file problem
    ----------------------------------------------------------------
    [v.2.0.1]
    21 Nov 2021

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
 
 // by cli
 gulp.task('default', function () {
   return gulp
     .src('src/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(concat('index.scss'))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./dist/'));
 });
