4c563f1d07918c5024a34294a9dd5200f69b1885
mikoloism <mikoloism.github@gmail.com>
2021-11-23 19:33:31 +0330

    [ 👍 ] reordered files by build file (from gulpfile)
    ----------------------------------------------------------------
    [v2.1.0]
    23 Nov 2021

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,21 @@ gulp.task('default', function () {
   return (
     gulp
       // .src('src/**/*.scss')
-      .src(['src/**/*.scss', '!src/**/_index.scss'])
+      // .src(['src/**/*.scss', '!src/**/_index.scss'])
+      .src([
+        'src/_at_the_head.scss',
+        'src/_dependencies.scss',
+        'src/utils/string/_length.scss',
+        'src/utils/string/_split.scss',
+        'src/utils/_units.scss',
+        'src/mixins/_flex.scss',
+        'src/mixins/_size.scss',
+        'src/mixins/_position.scss',
+        'src/mixins/_breakpoints.scss',
+        'src/mixins/_mixins.scss',
+        'src/mixins/_transform.scss',
+        'src/mixins/_style.scss',
+      ])
       .pipe(sourcemaps.init())
       .pipe(concat('index.scss'))
       .pipe(sourcemaps.write('.'))
